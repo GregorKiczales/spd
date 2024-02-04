d9670888f2dca821b0d1d77ca1fba4c59da68bed
GregorKiczales <38510095+GregorKiczales@users.noreply.github.com>
2024-02-04 11:08:45 -0800

    Update constants.rkt

--- constants.rkt
+++ constants.rkt
@@ -10,28 +10,15 @@
 (define @TAGS
   '(@assignment @cwl
                 @problem
-                @htdw @htdd
-                @htdf @signature @dd-template-rules @template-origin @template))
+                @htdw
+                @htdd @dd-template-rules
+                @htdf @signature @template-origin @template))
 
 (define    ATOMIC-TYPES '(Number Integer Natural String 1String Boolean Image Color Scene))
 (define PRIMITIVE-TYPES (append ATOMIC-TYPES '(KeyEvent MouseEvent)))
 
 (define DD-TEMPLATE-RULES
   '(atomic-non-distinct atomic-distinct one-of compound self-ref ref))
-#;
-(define TEMPLATE-ORIGINS
-  '(;<TypeName>      decomposition and possible structural recursion
-  
-    htdw-main    
-    fn-composition   ;use once for composition of 2 or more fns
-    try-catch
-    2-one-of
-    encapsulated
-    use-abstract-fn
-    genrec
-    bin-tree         ;must be used w/ genrec
-    arb-tree         ;must be used w/ genrec
-    accumulator))
 
 (define NON-TYPE-TEMPLATE-ORIGINS 
   '(htdw-main    
